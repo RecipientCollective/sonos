105ea623a6163971065268041b38e51505d708ba
mandrake <man@recipient.cc>
2011-03-05 15:53:52 +0100

    Debug videograb

--- videograb-opencv-sonos/src/testApp.cpp
+++ videograb-opencv-sonos/src/testApp.cpp
@@ -4,8 +4,8 @@
 //--------------------------------------------------------------
 void testApp::setup()
 {
-
-
+	
+	
 	
 #ifdef _USE_LIVE_VIDEO
 	camWidth = 640;
@@ -13,7 +13,7 @@ void testApp::setup()
 	vidGrabber.setVerbose(true);
 	vidGrabber.initGrabber(camWidth, camHeight, false);
 #else
-	vidPlayer.loadMovie("buio_aperto_ircam_oggetti.mov");
+	vidPlayer.loadMovie("retro-ir.mov");
 	vidPlayer.play();
 	camWidth = vidPlayer.getWidth();
 	camHeight = vidPlayer.getHeight();
@@ -21,135 +21,131 @@ void testApp::setup()
 	
 	
 	
-		
-		
-		colorImg.allocate(camWidth, camHeight);
-		grayImage.allocate(camWidth, camHeight);
-		grayBg.allocate(camWidth,camHeight);
-		grayDiff.allocate(camWidth,camHeight);
-		
-		bLearnBakground = false;
-		Threshold = 50;
-		
-		bThreshWithOpenCV = true;
-
-		ofSetFrameRate(60);
-
 	
-		colorz=1;
-		blobMax=3;
-		contour_min = 100;
-		scale_x = 1.0;
-		scale_y = 1.0;
-		mtrx = 1.0;
-		mtry = 1.0;
-		interface = true;
-		circle = true;
-		debug = false;
-		rectangle = false;
+	
+	colorImg.allocate(camWidth, camHeight);
+	grayImage.allocate(camWidth, camHeight);
+	grayBg.allocate(camWidth,camHeight);
+	grayDiff.allocate(camWidth,camHeight);
+	
+	bLearnBakground = true;
+	Threshold = 50;
+	
+	bThreshWithOpenCV = true;
+	
+	ofSetFrameRate(60);
+	
+	
+	colorz=1;
+	blobMax=2;
+	contour_min = 100;
+	scale_x = 1.0;
+	scale_y = 1.0;
+	mtrx = 1.0;
+	mtry = 1.0;
+	interface = true;
+	circle = true;
+	debug = false;
+	rectangle = false;
 }
 
 //--------------------------------------------------------------
 void testApp::update()
 {
-		ofBackground(100, 100, 100);
-		bool bNewFrame = false;
+	ofBackground(100, 100, 100);
+	bool bNewFrame = false;
+	
+#ifdef _USE_LIVE_VIDEO
+	vidGrabber.grabFrame();
+	bNewFrame = vidGrabber.isFrameNew();
+#else
+	vidPlayer.idleMovie();
+	bNewFrame = vidPlayer.isFrameNew();
+#endif
+	
+	
+	
+	if (bNewFrame){
 		
-	#ifdef _USE_LIVE_VIDEO
-		vidGrabber.grabFrame();
-		bNewFrame = vidGrabber.isFrameNew();
-	#else
-		vidPlayer.idleMovie();
-		bNewFrame = vidPlayer.isFrameNew();
-	#endif
-	
-	
-	
-		if (bNewFrame){
-		
-		#ifdef _USE_LIVE_VIDEO
-			colorImg.setFromPixels(vidGrabber.getPixels(), camWidth, camHeight);
-		#else
-            colorImg.setFromPixels(vidPlayer.getPixels(), camWidth,camHeight);
-		#endif
-			
+#ifdef _USE_LIVE_VIDEO
+		colorImg.setFromPixels(vidGrabber.getPixels(), camWidth, camHeight);
+#else
+		colorImg.setFromPixels(vidPlayer.getPixels(), camWidth,camHeight);
+#endif						
 
-			
-			
-			grayImage = colorImg;
-			if (bLearnBakground == true){
-				grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
-				bLearnBakground = false;
-			}
-			
-			// take the abs value of the difference between background and incoming and then threshold:
-			grayDiff.absDiff(grayBg, grayImage);
-			grayDiff.threshold(Threshold);
-			}
+		grayImage = colorImg;
+		if (bLearnBakground == true){
+			grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
+			bLearnBakground = false;
+		}
 		
-
+		// take the abs value of the difference between background and incoming and then threshold:
+		grayDiff.absDiff(grayBg, grayImage);
+		grayDiff.threshold(Threshold);
+	
 		//update the cv image
-	grayImage.flagImageChanged();
-
-    // find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
-    // also, find holes is set to true so we will get interior contours as well....
-	/*
-	 ofxCvContourFinder::findContours( ofxCvGrayscaleImage&  input,
-		int minArea,
-		int maxArea,
-		int nConsidered,
-		bool bFindHoles,
-		bool bUseApproximation
-	 )
-	 */
+		//grayImage.flagImageChanged();
+		
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		/*
+		 ofxCvContourFinder::findContours( ofxCvGrayscaleImage&  input,
+		 int minArea,
+		 int maxArea,
+		 int nConsidered,
+		 bool bFindHoles,
+		 bool bUseApproximation
+		 )
+		 */
+		
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		contourFinder.findContours(grayDiff, contour_min, (camWidth*camHeight)/3, blobMax, false, true);
 	
-	// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
-	// also, find holes is set to true so we will get interior contours as well....
-	contourFinder.findContours(grayDiff, contour_min, (camWidth*camHeight)/3, blobMax, false, true);
-	
+	}
 }
 
 //--------------------------------------------------------------
 void testApp::draw()
 {		
-
+	
 	ofPushMatrix();
 	ofScale(scale_x, scale_y, 1.0);
 	ofTranslate(mtrx, mtry, 1.0);
-
+	
 	if (debug) {
-		 
-		
-			colorImg.draw(20,20);
-			grayImage.draw(360,20);
-			grayBg.draw(20,280);
-			grayDiff.draw(360,280);
-			
-			// then draw the contours:
-			
-			ofFill();
-			ofSetColor(0x333333);
-			ofRect(360,540,320,240);
-			ofSetColor(0xffffff);
-			
-			// we could draw the whole contour finder
-			//contourFinder.draw(360,540);
-			
-			// or, instead we can draw each blob individually,
-			// this is how to get access to them:
-			for (int i = 0; i < contourFinder.nBlobs; i++){
-				contourFinder.blobs[i].draw(360,540);
-			}
 		
 		
-
-
+		colorImg.draw(20,20);
+		grayImage.draw(360,20);
+		grayBg.draw(20,280);
+		grayDiff.draw(360,280);
+		
+		// then draw the contours:
+		
+		ofFill();
+		ofSetColor(0x333333);
+		ofRect(360,540,320,240);
+		ofSetColor(0xffffff);
+		
+		// we could draw the whole contour finder
+		//contourFinder.draw(360,540);
+		
+		// or, instead we can draw each blob individually,
+		// this is how to get access to them:
+		for (int i = 0; i < contourFinder.nBlobs; i++){
+			contourFinder.blobs[i].draw(360,540);
+		}
+		
+		
+		
+		
 		
 	} else {
-			ofSetColor(205, 205, 205);
-			grayDiff.draw(0,0, camWidth, camHeight);
-			//colorImg.draw(20,20, camWidth, camHeight);
-			//grayImage.draw(0, 0, camWidth, camHeight);
+		ofSetColor(205, 205, 205);
+		grayDiff.draw(0,0, camWidth, camHeight);
+		//colorImg.draw(20,20, camWidth, camHeight);
+		//grayImage.draw(0, 0, camWidth, camHeight);
 		
 		for(int i = 0; i < contourFinder.blobs.size(); i++) {
 			
@@ -183,7 +179,7 @@ void testApp::draw()
 			
 			//ofSetColor(255, 255, 255);
 			if (circle) {
-				float raggio = (blobheight >= blobwidth ? blobheight : blobwidth) / 2.0;
+				float raggio = (blobheight >= blobwidth ? blobheight : blobwidth) / 1.5;
 				ofCircle( cx, cy, raggio);
 			}
 			if (rectangle) {
@@ -192,14 +188,14 @@ void testApp::draw()
 			
 			
 			//if (interface) {
-				//ofSetColor(255, 255, 255);
-				//ofDrawBitmapString("blob "+ ofToString(i, 0) + ": try " + ofToString(cy, 2) + " /trx " + ofToString(cx, 2) + " /area " + ofToString(blobarea, 0) , 20, 200+i*10 );
+			//ofSetColor(255, 255, 255);
+			//ofDrawBitmapString("blob "+ ofToString(i, 0) + ": try " + ofToString(cy, 2) + " /trx " + ofToString(cx, 2) + " /area " + ofToString(blobarea, 0) , 20, 200+i*10 );
 			//}
 		}
 		
 	}
-
-
+	
+	
 	ofPopMatrix();
 	
 	ofSetColor(255, 255, 255);
@@ -214,14 +210,14 @@ void testApp::draw()
 		ofDrawBitmapString("scale: " + ofToString(scale_x) + "," + ofToString(scale_y) + " (press: a z)", 20,160);
 		ofDrawBitmapString("translate: " + ofToString(mtrx) + "," + ofToString(mtry) + " (press: ARROWS)", 20,190);			
 		ofDrawBitmapString("d: debug mode. c: toggle circle. r: reset scale and translate. ,: video settings", 20,220);
-
+		
 	}
 }
 
 
 //--------------------------------------------------------------
 void testApp::exit(){
-//magari c' da chiudere la cam o i video da verificare;
+	//magari c' da chiudere la cam o i video da verificare;
 }
 
 //--------------------------------------------------------------
@@ -232,8 +228,8 @@ void testApp::keyPressed (int key)
 		case ' ':
 			bLearnBakground = true;
 			
-		break;
-	
+			break;
+			
 		case '>':
 			blobMax ++;
 			break;
@@ -252,11 +248,11 @@ void testApp::keyPressed (int key)
 		case 'w':
 			
 			break;
-
+			
 		case 's':
 			bThreshWithOpenCV = !bThreshWithOpenCV;
 			break;
-
+			
 		case 'x':
 			
 			break;
@@ -273,30 +269,30 @@ void testApp::keyPressed (int key)
 		case OF_KEY_RIGHT:
 			mtrx++;
 			break;
-
+			
 		case 'f':
 			bFullscreen = !bFullscreen;
 			if (!bFullscreen){
-			ofSetWindowShape(1024,768);
-			ofSetFullscreen(false);
-			// figure out how to put the window in the center:
-			int screenW = ofGetScreenWidth();
-			int screenH = ofGetScreenHeight();
-			ofSetWindowPosition(screenW/2-1024/2, screenH/2-768/2);
+				ofSetWindowShape(1024,768);
+				ofSetFullscreen(false);
+				// figure out how to put the window in the center:
+				int screenW = ofGetScreenWidth();
+				int screenH = ofGetScreenHeight();
+				ofSetWindowPosition(screenW/2-1024/2, screenH/2-768/2);
 			}
 			else if (bFullscreen == 1){
-			ofSetFullscreen(true);
+				ofSetFullscreen(true);
 			}
 			break;
 			
 		case '1':
 			colorz=1;
 			break;
-		
+			
 		case '2':
 			colorz=2;
 			break;
-				
+			
 		case '3':
 			colorz=3;
 			
@@ -341,11 +337,11 @@ void testApp::keyPressed (int key)
 		case 'v':
 			break;
 		case ',':
-	#ifdef _USE_LIVE_VIDEO
+#ifdef _USE_LIVE_VIDEO
 			vidGrabber.videoSettings();
-	#else
+#else
 			//
-	#endif
+#endif
 			break;
 	}
 }
