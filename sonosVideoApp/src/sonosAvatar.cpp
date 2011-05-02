0082b62efec1492dc26b05bfb25c8464c0d4e703
Davide Rambaldi <davide.rambaldi@gmail.com>
2011-05-02 10:30:49 +0200

    sonosAvatar step1

---
+++ sonosVideoApp/src/sonosAvatar.cpp
@@ -0,0 +1,11 @@
+/*
+ *  sonosAvatar.cpp
+ *  sonosVideoApp
+ *
+ *  Created by tucano on 5/2/11.
+ *  Copyright 2011 IFOM-FIRC. All rights reserved.
+ *
+ */
+
+#include "sonosAvatar.h"
+
---
+++ sonosVideoApp/src/sonosAvatar.h
@@ -0,0 +1,22 @@
+/*
+ *  sonosAvatar.h
+ *  sonosVideoApp
+ *
+ *  Created by tucano on 5/2/11.
+ *  
+ *  Un Avatr per ogni sonosBlob.
+ *  
+ */
+
+class sonosAvatar
+{
+public:
+	float x;
+	float y;
+	
+	sonosAvatar();
+	sonosAvatar(float ax, float ay);
+	
+	void draw();
+	
+};
