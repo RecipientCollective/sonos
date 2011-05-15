c010dfdd3eccce5614790a5acf593e51aa9b8bf4
mandrake <man@recipient.cc>
2011-05-16 01:10:47 +0200

    added osc message reciver rutines

--- sonosVideoApp/src/sonosApp.cpp
+++ sonosVideoApp/src/sonosApp.cpp
@@ -782,40 +782,16 @@ void sonosApp::OscUpdate()
 		
 		// check for mouse moved message
 		// midi type note channel 1
-		if ( m.getAddress() == "/midi/note/1" )
+		if ( m.getAddress() == "" )
 		{
 			// both the arguments are int32's
 			//example= m.getArgAsInt32( 0 );
 			//example= m.getArgAsInt32( 1 );
 			
-			string msg_string;
-			msg_string = m.getAddress();
-			msg_string += ": ";
-			for ( int i=0; i<m.getNumArgs(); i++ )
-			{
-				// get the argument type
-				msg_string += m.getArgTypeName( i );
-				msg_string += ":";
-				// display the argument - make sure we get the right type
-				if( m.getArgType( i ) == OFXOSC_TYPE_INT32 )
-					msg_string += ofToString( m.getArgAsInt32( i ) );
-				else if( m.getArgType( i ) == OFXOSC_TYPE_FLOAT )
-					msg_string += ofToString( m.getArgAsFloat( i ) );
-				else if( m.getArgType( i ) == OFXOSC_TYPE_STRING )
-					msg_string += m.getArgAsString( i );
-				else
-					msg_string += "unknown";
-			}
-			// add to the list of strings to display
-			msg_strings[current_msg_string] = msg_string;
-			timers[current_msg_string] = ofGetElapsedTimef() + 5.0f;
-			current_msg_string = ( current_msg_string + 1 ) % NUM_MSG_STRINGS;
-			// clear the next line
-			msg_strings[current_msg_string] = "";
 		}
-		// check for mouse button message
 		else if ( m.getAddress() == "/midi/note/2" )
 		{
+			//argomenti mesaggio di nota "int:note  float:velocity  int:trigger"
 			// the single argument is a string
 			//mouseButtonState = m.getArgAsString( 0 ) ;
 			
@@ -975,10 +951,10 @@ void sonosApp::sonosDraw()
 //-----------OSC DRAW---------------------------------------------------
 void sonosApp::OscDraw()
 {	
-	//for ( int i=0; i<NUM_MSG_STRINGS; i++ )
-	//{
-	//	ofDrawBitmapString( msg_strings[i], 10, 40+15*i );
-	//}
+	for ( int i=0; i<NUM_MSG_STRINGS; i++ )
+	{
+		ofDrawBitmapString( msg_strings[i], 10, 40+15*i );
+	}
 	
 	
 }
